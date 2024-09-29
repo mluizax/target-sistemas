b6cb00a8c234a3ec9a5e2c575b89d57a0764afba
Maria Luíza Ottwil <94000710+mluizax@users.noreply.github.com>
2024-09-29 18:13:14 -0300

    Technical Question 5

---
+++ t5.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    let invertedString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        invertedString += str[i];
+    }
+    return invertedString;
+}
+
+console.log(reverseString("Luiza"));
