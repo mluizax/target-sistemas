4ef8e9b328f59d915c599746209a30a27b0adc19
Maria Luíza Ottwil <94000710+mluizax@users.noreply.github.com>
2024-09-29 18:04:20 -0300

    Technical Question 1

---
+++ t1.js
@@ -0,0 +1,13 @@
+let index = 13;
+let sum = 0;
+let k = 0;
+
+while (k < index) {
+    k = k + 1;
+    sum = sum + k;
+}
+
+console.log(sum); 
+
+/* Q: Ao final do processamento, qual será o 
+valor da variável SOMA (sum)? 91 */
